933de3e6f7d9a544bdeea3f51043db94eeb1bdc5
wendyeldah <s223736923@deakin.edu.au>
2024-03-25 23:20:35 +1100

    Final commit

---
+++ index.js
@@ -0,0 +1,11 @@
+express = require("express")
+var app = express()
+
+
+app.use(express.static(__dirname+'/public_html'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+var port = process.env.port || 3000;
+app.listen(port,()=>{
+console.log("App listening to: "+port)
+})
